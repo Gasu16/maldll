75c4ae9bb057d394c87a04b020a4fe1163501e3c
Gasu16 <rosario.matteog@gmail.com>
2025-02-19 15:36:50 +0100

    add internet conn

--- maldll/dllmain.cpp
+++ maldll/dllmain.cpp
@@ -1,5 +1,10 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <windows.h>
+#include <wininet.h>
+#include <winsock.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 extern "C" __declspec(dllexport) void CALLBACK mydllmain()
 {
@@ -24,6 +29,12 @@ extern "C" __declspec(dllexport) void CALLBACK createfile() {
     strncpy_s(szBuffer, sizeof(szBuffer), text, strlen(text));
     WriteFile(hFile, szBuffer, strlen(szBuffer), &dwBytesWritten, NULL);
     MessageBox(0, L"File Written", L"FileW", 0);
+}
+
+extern "C" __declspec(dllexport) void CALLBACK internet_conn() {
+    HANDLE conn = InternetOpenA("Mozilla/4.1337", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+    HANDLE conn_url = InternetOpenUrlA(conn, "https://www.google.com", 0, 0, INTERNET_FLAG_RAW_DATA, 0);
+    MessageBox(0, L"Connessione riuscita", L"Conn", 0);
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
